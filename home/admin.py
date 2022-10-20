3c0fd7106057568891b05d4c8b4ab6ff864060fb
Crowdbotics <engineering@crowdbotics.com>
2022-10-20 10:30:31 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Addresseryuplivgd
+
+admin.site.register(Addresseryuplivgd)
 
 # Register your models here.
--- tst_tc185_gqglavcqp_39536/wsgi.py
+++ tst_tc185_gqglavcqp_39536/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tst_tc185_gqglavcqp_39536.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tst_tc185_gqglavcqp_39536.settings")
 
 application = get_wsgi_application()
